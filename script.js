4e1bfcd8fabc186238861ca776898008070c9a70
nico-fb <nicofb31@gmail.com>
2023-05-07 18:00:59 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+    function toggleNav () {      
+
+        let navElems = document.getElementsByClassName("navbar-li-collapse");
+        
+        if (navElems[0].style.display == "none" ){
+            for (var i=0;i<navElems.length;i+=1){
+                navElems[i].style.display = "block";
+            }
+        } else {
+            for (var i=0;i<navElems.length;i+=1){
+                navElems[i].style.display = "none";
+            }
+        }
+    }
+
+    function windowResizeHandler () {
+
+        let navElems = document.getElementsByClassName("navbar-li-collapse");
+
+        if (window.innerWidth > 750){
+            for (var i=0;i<navElems.length;i+=1){
+                navElems[i].style.display = "block";
+            }
+        } else {
+            for (var i=0;i<navElems.length;i+=1){
+                navElems[i].style.display = "none";
+            }
+        }
+    }
+
+    window.addEventListener("resize", windowResizeHandler);
