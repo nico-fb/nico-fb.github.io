e48a47e562b8aafea18ce881957aca89dfe7fd5d
nico-fb <nicofb31@gmail.com>
2023-06-28 14:33:25 -0400

    v1

--- script.js
+++ script.js
@@ -30,3 +30,25 @@
     }
 
     window.addEventListener("resize", windowResizeHandler);
+
+    function toggleProjectsPopup(index) {
+        let popups = document.getElementsByClassName("projects-popup");
+        let popup = popups[popups.length - index];
+
+        popup.classList.toggle("open-projects-popup");
+        popup.scrollTop = 0;
+        document.getElementsByClassName("popup-background")[0].classList.toggle("open-popup-background");
+        document.getElementsByClassName("body")[0].classList.toggle("open-popup-body");
+
+    }
+
+    window.onclick = function(event) {
+        if (event.target == document.getElementsByClassName("popup-background")[0]) {
+            let popups = document.getElementsByClassName("projects-popup");
+            for (let i = 0; i < popups.length; i++){
+                popups[i].classList.remove("open-projects-popup");
+            }
+            document.getElementsByClassName("popup-background")[0].classList.remove("open-popup-background");
+            document.getElementsByClassName("body")[0].classList.toggle("open-popup-body");
+        }
+    }
